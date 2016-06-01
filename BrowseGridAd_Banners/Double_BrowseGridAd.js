335539657f45094480b45d0fda483621148bd817
juanmriosv <juanmanuelriosv@gmail.com>
2016-06-01 12:38:09 -0400

    NJ - Single Updated - Double 1st commit

--- BrowseGridAd_Banners/Double_BrowseGridAd.js
+++ BrowseGridAd_Banners/Double_BrowseGridAd.js
@@ -1,13 +1,13 @@
-// ALL PAGES BROWSE GRID 1 GRID AD BANNER
+// ALL PAGES BROWSE GRID 2 GRID AD BANNER
 $(window).load(function() {
 
 	//POSITION
-	var pos = 3;
+	var pos = 4;
 	//IMAGE URL
-	var urlIMG = "https://www.c21stores.com/media/W1siZiIsIjIwMTYvMDUvMjcvMDkvMDkvMzcvNjc4LzM3MHg1NTMuanBnIl1d/370x553.jpg?sha=cdce0addc7721d54";
-	//LINK TEXT
+	var urlIMG = "https://c21stores-weblinc.netdna-ssl.com/media/W1siZiIsIjIwMTYvMDEvMjgvMTAvNDcvMDMvNTIyLzAyV2VlazNfQnJvd3NlR3JpZF9fRW5sYXJnZWRfaW1hZ2VzLmpwZyJdXQ/02Week3_BrowseGrid__Enlarged_images.jpg?sha=e2648a7818316693";
+	//CTA TEXT
 	var textLink = "SHOP NOW";
-	//LINK URL
+	//CTA URL
 	var urlLINK = "https://www.c21stores.com/pages/gift-cards";
 	
 	// WHEN FINISH RESIZING WINDOWS MATCH HEIGHT
@@ -17,12 +17,12 @@ $(window).load(function() {
     	clearTimeout(FixHeights);
     	FixHeights = setTimeout(function() {
 		// console.log('yay it works!');
-		AdClassAll_Single();	
+		AdClassAll_Double();	
     	}, 100);
 	};
 
 	$('.product-grid li:nth-of-type('+ realPos +')').after(
-		'<li class="product-grid__cell product-grid__cell--1 jmrv-product-grid__cell--1">'+
+		'<li class="product-grid__cell product-grid__cell--2 jmrv-product-grid__cell--2">'+
 		'<div class="product-grid-content-block" style="background-image: url('+ urlIMG +');">'+
 		'<div class="product-grid-content-block__image"></div>'+
 		'<div class="content-block-content content-block-content--center content-block-content--bottom">'+
@@ -36,12 +36,12 @@ $(window).load(function() {
 		'</li>'
 	);
 	
-	function AdClassAll_Single(){
+	function AdClassAll_Double(){
 		var newHeight1 = $('.product-grid li:nth-of-type('+ (realPos + 2) + ')').height();
 		console.log($('.product-grid li:nth-of-type('+ (realPos + 2) + ')'));
 		console.log($('.product-grid li:nth-of-type(3)'));		
-		$('.jmrv-product-grid__cell--1').height(newHeight1);
-	}AdClassAll_Single();
+		$('.jmrv-product-grid__cell--2').height(newHeight1);
+	}AdClassAll_Double();
 
 });
-// END ALL PAGES BROWSE 1 GRID GRID AD BANNER
+// DONE ALL PAGES BROWSE 2 GRID GRID AD BANNER
