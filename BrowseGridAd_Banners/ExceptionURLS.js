171f84d9b05eb9817903fc8cd3c2f6d9e9d5a0b3
juanmriosv <juanmanuelriosv@gmail.com>
2016-06-01 16:31:45 -0400

    NJ-Exception URL

--- BrowseGridAd_Banners/ExceptionURLS.js
+++ BrowseGridAd_Banners/ExceptionURLS.js
@@ -12,23 +12,13 @@ exceptThisUR(URLS);
 function exceptThisUR(params) {
   for (i=0; i<params.length; i++) {
     console.log(params[i]);
-    if(window.location.href === params[i]){
-    	$('.product-grid__cell--1').fadeOut();	
-    }
-    
+	if(window.location.href.indexOf(params[i]) > -1) {
+    	$('.product-grid__cell--1').fadeOut();    	
+    	alert("your url contains the string "+params[i]);
+    }    
   }
 }
 
 });
 // END URLS EXCEPTIONS
 
-
-//GRID AD BANNER GLOBAL EXCEPTION
-	if(window.location.href == "https://www.c21stores.com/categories/gifts-accessories" || 
-	window.location.href == "https://www.c21stores.com/categories/gifts-dress-shirts-ties" || 
-	window.location.href == "https://www.c21stores.com/categories/gifts-cologne" ||
-	window.location.href == "https://www.c21stores.com/categories/gifts-polos-shirts" ||
-	window.location.href == "https://www.c21stores.com/categories/gifts-tech"){
- 		$('.product-grid__cell--1').fadeOut();
-	}
-	//END GRID AD BANNER GLOBAL EXCEPTION
