096e376afcdc2aa91dbd252bc5370856967510d5
Juan Manuel Rios Villanueva <juanmanuelriosv@gmail.com>
2017-01-26 12:20:53 -0500

    Global JS - Inventory ATB message down W01
    Hover Function on the Add To Bag Button down for W01 FEB.
    End of Inventory message over the "Add To Bag" button

--- GLOBAL_Javascript.js
+++ GLOBAL_Javascript.js
@@ -1,5 +1,5 @@
-// 90% ADDED and INVENTORY
-// MASTER SCRIPTING JAN 20 2017
+// Hover_message Add to Bag Button Down
+// MASTER SCRIPTING JAN 26 2017
 // contact: jrios@c21stores.com
 
 
@@ -8,7 +8,6 @@ document.addEventListener( "DOMContentLoaded", uniqueClasses, false );
 document.addEventListener( "DOMContentLoaded", pdpVideos, false );
 document.addEventListener( "DOMContentLoaded", pdpTieredPrice, false );
 document.addEventListener( "DOMContentLoaded", pdpKnivesPDF, false );
-document.addEventListener( "DOMContentLoaded", Hover_message_Button, false );
 
 // FIREING FUNCTIONS ON QUICK VIEW
 $(document).on( 'click', '.product-summary__quickview a', function(){
@@ -1474,67 +1473,6 @@ function pdpKnivesPDF(){
 	
 };
 // END KNIVES PDF KNIVES PDF KNIVES PDF KNIVES PDF KNIVES PDF
-
-
-// HOVER MESSAGE ADD TO BAG BUTTON
-
-
-function Hover_message_Button(){    
-
-		$('.product-details__add-to-cart-form > section').after(
-			'<div class="jm_button_message" style="display:none; font-size: 14px; color: #e21f26; padding: 0 0 5%; text-rendering: optimizeLegibility; -webkit-transition: width 2s; transition: width 2s;">' + 
-			'<b>SHIPPING NOTICE:</b> Orders Placed 1/21-1/29 Will Ship Starting <b>1/30</b>' +
-			'</div>'			
-		);	
-		
-		
-if ($(window).width() >= 700){ 
-	Hover_message_Button_desktop();
-}
-else{
-	Hover_message_Button_mobile();
-}
-	function Hover_message_Button_desktop(){
-		console.log('desktop');
-
-		var check_button =  $('.button--with-arrow').attr("value");
-		if(check_button == "add_to_cart"){
-		   console.log("winner!");
-			$('.button--with-arrow, .jm_button_message').hover(
-			   function () {
-				  $('.jm_button_message').stop().fadeIn();
-			   }, 
-			
-			   function () {
-				  $('.jm_button_message').stop().fadeOut();
-			   }
-			);           
-	   
-		}
-	};
-	function Hover_message_Button_mobile(){
-		console.log('mobile');
-		$(window).scroll(function() {
-			var $rightPlace_button = $(".product-details__add-to-cart-action");
-			var actual_height = $(window).height();
-	
-
-			window_offset_button = $rightPlace_button.offset().top - $(window).scrollTop();
-	
-			console.log(actual_height);    
-			console.log(window_offset_button);    
-	
-			if((window_offset_button < actual_height)){
-			  $('.jm_button_message').fadeIn(1000);
-			} 
-			else{
-			  return false;
-			} 
-		});
-
-	};	
-};
-// END HOVER MESSAGE ADD TO BAG BUTTON   
 
 
 //QUANITY ADDING NUMBERS
