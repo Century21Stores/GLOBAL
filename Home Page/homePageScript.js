f61a69de1572030f8e9d1c940ef67def99bab8e6
Rios <jrios@c21stores.com>
2017-12-12 09:37:18 -0500

    W46A Fix for Autoplay on scroll in Home Pages

--- Home Page/homePageScript.js
+++ Home Page/homePageScript.js
@@ -1,4 +1,4 @@
-// W47B Last day 12/20 for Next-Day Shipping!* - JuanMa 12.07.17
+// W46A Ship gifts in time for Christmas - Last chance for Standard & 3-Day Shipping!* - JuanMa 12.07.17
 
 $(document).ready(function(){
     $('.jm-HomePage5 .jm-cloneIt, .jmrv-hp3 .jm-cloneIt').each(function(){
@@ -17,7 +17,7 @@ console.log(new_data);
 $('.branded-card-header').after(
 	'<section class="jm-home-page-header">'+
 	'<p class="jm-mobile-HPheader-l1">Ship gifts in time for Christmas</p>'+
-	'<h3><b>Last day 12/20</b> for <b>Next-Day Shipping!<span class="jm-asterisk">*</span></b></h3>'+
+	'<h3>Last days for <b>Standard & 3-Day Shipping!<span class="jm-asterisk">*</span></b></h3>'+
 	'<p class="content-block-content__action content-block-content__action--dark-text"><a class="text jm-mobile-HPheader-cta" href="https://www.c21stores.com/pages/shipping-information">LEARN MORE</a></p>'+
   '<p class="jm-desktop jm-HPH-fine-print"><span class="jm-asterisk">*</span>Dates vary by location.</span></p>'+
   '</section>'
@@ -26,51 +26,52 @@ $('.branded-card-header').after(
 
 // VIDEO TO PLAY ON SCROLL
 
-var jmplayer2;
+// API
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// API CALL
-function onYouTubePlayerAPIReady() {
-  jmplayer2 = new YT.Player('jm-video-OnScroll', {
-    events: {
-      'onReady': onPlayerReady
+//Holding All video players here
+var players = {};
+var playerCount = 0;
+
+var iframes = $('.jm_video_OnScroll');
+
+jQuery(document).ready(function($) {
+    onYouTubeIframeAPIReady = function() {
+        iframes.each(function( index ) {
+            players[index] = new YT.Player( this, {
+              events: {
+                  'onReady': onPlayerReady
+                }
+            });
+        });
+
     }
-  });
-}
+    function onPlayerReady(event) {
+      playerCount++;
+        if(playerCount == iframes.size()){
+            assignWaypoints();
+        }
+    }
+    function assignWaypoints() {
+        iframes.each(function( index ) {
+           $(this).waypoint(function(direction) {
+                if(direction=="up"){
+                    players[index].playVideo();
+                } else {
+                    players[index].stopVideo();
+                }
+            }, { offset: '-75%' });
+            $(this).waypoint(function(direction) {
+                if(direction=="up"){
+                    players[index].stopVideo();;
+                } else {
+                    players[index].playVideo();
+                }
+            }, { offset: '75%' });
 
-function onPlayerReady(event) {
-
-  var playButton = document.getElementById("jm-toVideo-play");
-  var stopButton = document.getElementById("jm-toVideo-stop");
-
-
-
-	$(window).scroll(function() {
-
-		var $message2 = $(".video-content-block");
-
-
-		window_offset2 = $message2.offset().top - $(window).scrollTop();
-
-			console.log(window_offset2);
-
-
-			if((window_offset2 > -700) && (window_offset2 < 1040)){
-				console.log('arrive2!!');
-				$(".video-content-block").addClass('flying_from_left2');
-		    	jmplayer2.playVideo();
-			}
-			else{
-				jmplayer2.pauseVideo();
-			}
-
-	});
-
-//
-//     playButton.addEventListener("click", function() {
-//     	jmplayer2.playVideo();
-//   	});
-//     stopButton.addEventListener("click", function() {
-//     	jmplayer2.stopVideo();
-//   	});
-
-}
+        });
+    }
+});
